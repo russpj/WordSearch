6fce01b6efe134c75382d35fcfe494fe75d459d5
Russ Paul-Jones <russ@paul-jones.org>
2020-04-27 16:33:57 -0700

    Finding all words.
    Even the short ones

--- Tests.py
+++ Tests.py
@@ -4,19 +4,40 @@
 
 from WordSearch import WordSearchSolver
 
+letters = [
+		['a','d','r','e'],
+		['e','i','e','s'],
+		['n','r','t','h'],
+		['s','g','u','o']
+	]
+
+def PrintGrid(letters):
+	for row in letters:
+		print(row)
+
 def ShowSearchResult(solver, word):
 	print('Looking up {word}: {result}'.format(result=solver.FindWord(word), word=word))
 
 
 def Main():
 	print('Running tests')
-	testSolver = WordSearchSolver('studentdictionary.txt')
+	testSolver = WordSearchSolver('studentdictionary.txt', letters)
 	print('First word is {word}.'.format(word=testSolver.dictionary[0]))
 	ShowSearchResult(testSolver, 'pet')
 	ShowSearchResult(testSolver, 'pre')
 	ShowSearchResult(testSolver, 'a')
 	ShowSearchResult(testSolver, 'zzzzzz')
 	ShowSearchResult(testSolver, '')
+	print()
+	PrintGrid(letters)
+
+	print('Looking for words')
+	foundWords = []
+	for word in testSolver.FindAllWords():
+		if word not in foundWords:
+			print (word)
+			foundWords.append(word)
+	print('Found {number} words.'.format(number=len(foundWords)))
 
 if __name__ == '__main__':
 	Main()
--- WordSearch.py
+++ WordSearch.py
@@ -6,9 +6,16 @@ from enum import Enum
 from bisect import bisect_left
 
 class WordSearchSolver:
-	def __init__(self, dictionaryName='', **kwargs):
+	def __init__(self, dictionaryName='', letters=[], **kwargs):
 		if dictionaryName:
 			self.InitDictionary(dictionaryName)
+		self.letters=letters
+		self.numRows = len(letters)
+		if self.numRows > 0:
+			self.numCols = len(letters[0])
+		else:
+			self.numCols = 0
+		self.path = []
 
 	def InitDictionary(self, name):
 		with open(name) as dictionaryFile:
@@ -33,3 +40,46 @@ class WordSearchSolver:
 			else:
 				match = self.Match.NoMatch
 		return match, matchedWord
+
+	def WordFromPath(self):
+		word = ''
+		for cell in self.path:
+			word+=(self.letters[cell[0]][cell[1]])
+		return word
+
+	def InRange(self, cell):
+		row = cell[0]
+		col = cell[1]
+		return (row >= 0 and row < self.numRows and col >=0 and col < self.numCols)
+
+	def Cells(self, cell):
+		row = cell[0]
+		col = cell[1]
+		for drow in range(-1, 2):
+			for dcol in range(-1, 2):
+				yield [row+drow, col+dcol]
+
+	def FindWords(self, cell):
+		if not self.InRange(cell):
+			return
+		if cell in self.path:
+			return
+		self.path.append(cell)
+		word = self.WordFromPath()
+		match, matchWord = self.FindWord(word)
+		if match == self.Match.NoMatch:
+			self.path.pop()
+			return
+		
+		if match == self.Match.ExactMatch:
+			yield word
+
+		for cell in self.Cells(cell):
+			yield from self.FindWords(cell)
+		self.path.pop()
+		return
+
+	def FindAllWords(self):
+		for row in range(len(self.letters)):
+			for col in range(len(self.letters[row])):
+				yield from self.FindWords([row, col])
