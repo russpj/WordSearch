37412b1e2f6a19d68b9b466307d259ec09c2dbd5
Russ Paul-Jones <russ@paul-jones.org>
2020-04-27 15:23:35 -0700

    Add some simple tests for binary search in dictionary

--- Tests.py
+++ Tests.py
@@ -2,9 +2,21 @@
 
 # A test driver for WordSearch, using only console output
 
+from WordSearch import WordSearchSolver
+
+def ShowSearchResult(solver, word):
+	print('Looking up {word}: {result}'.format(result=solver.FindWord(word), word=word))
+
+
 def Main():
 	print('Running tests')
-	pass
+	testSolver = WordSearchSolver('studentdictionary.txt')
+	print('First word is {word}.'.format(word=testSolver.dictionary[0]))
+	ShowSearchResult(testSolver, 'pet')
+	ShowSearchResult(testSolver, 'pre')
+	ShowSearchResult(testSolver, 'a')
+	ShowSearchResult(testSolver, 'zzzzzz')
+	ShowSearchResult(testSolver, '')
 
 if __name__ == '__main__':
 	Main()
--- WordSearch.py
+++ WordSearch.py
@@ -1,1 +1,35 @@
+# WordSearch.py
 
+# Implements Boggle style word search on a grid of letters
+
+from enum import Enum
+from bisect import bisect_left
+
+class WordSearchSolver:
+	def __init__(self, dictionaryName='', **kwargs):
+		if dictionaryName:
+			self.InitDictionary(dictionaryName)
+
+	def InitDictionary(self, name):
+		with open(name) as dictionaryFile:
+			self.dictionary = dictionaryFile.read().splitlines()
+
+	class Match(Enum):
+		ExactMatch = 1
+		PrefixMatch = 2
+		NoMatch = 9
+
+	def FindWord(self, word):
+		index = bisect_left(self.dictionary, word)
+		if index == len(self.dictionary):
+			match = self.Match.NoMatch
+			matchedWord = ''
+		else:
+			matchedWord = self.dictionary[index]
+			if word == matchedWord:
+				match = self.Match.ExactMatch
+			elif word == matchedWord[:len(word)]:
+				match = self.Match.PrefixMatch
+			else:
+				match = self.Match.NoMatch
+		return match, matchedWord
